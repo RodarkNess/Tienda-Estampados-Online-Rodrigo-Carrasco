ec77efdd02813fc27341b024b769a2d71e201e82
Rodrigo Carrasco <rodrigo.carrasco56@inacapmail.cl>
2026-09-07 13:20:41 -0300

    Una línea
    Una línea

--- ventas/urls.py
+++ ventas/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path("productos/nuevo/", views.producto_create, name="producto_create"),
     path("productos/<int:pk>/editar/", views.producto_update, name="producto_update"),
     path("productos/<int:pk>/eliminar/", views.producto_delete, name="producto_delete"),
-
     path("ventas/nueva/", views.venta_create, name="venta_create"),
     path("ventas/", views.venta_list, name="venta_list"),
     path("ventas/<int:pk>/boleta/", views.boleta_detail, name="boleta_detail"),
